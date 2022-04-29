57d3574d5ff379183bd3de5552db42a60db90420
LAPTOP-OF7S8HM0\Mel Liza <limbagomelliza31@gmail.com>
2022-04-29 12:19:00 +0800

    changes

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,20 +1,46 @@
-import React from "react";
+import React,{ useEffect, useState } from "react";
 import { useAuth } from "../services/AuthProvider";
 import "./dashboard.css";
 import SchoolDistrictReportComponent from "./dashboard/SchoolDistrictReportComponent";
 import PersonalSummary from "./dashboard/PersonnelSumary";
 import DesignationSummary from "./dashboard/DesignationSummary";
-import Tabs from "./dashboard/Tabs";
+import { Tab, Tabs, TabList, TabPanel } from 'react-bootstrap';
 
 export default function Dashboard() {
+  const [key, setKey] = useState('personnel');
   return (
-    <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+    <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4 py-5">
       <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <div>DASHBOARD</div>
       </div>
-      {/* <SchoolDistrictReportComponent />
-      <hr className="line-division"></hr> */}
-      <Tabs />
+      <SchoolDistrictReportComponent />
+      <hr className="line-division"></hr>
+    {/* <Tabs
+      id="controlled-tab-example"
+      activeKey={key}
+      onSelect={(k) => setKey(k)}
+      className="mb-5"
+    >
+      <Tab eventKey="personnel" title="Summary of Div. Personnel">
+        <PersonalSummary />
+      </Tab>
+      <Tab eventKey="position" title="Summary of Position">
+        <DesignationSummary />
+      </Tab>
+      <Tab eventKey="salary" title="Total Salary Budget">
+        <PersonalSummary />
+      </Tab>
+      <Tab eventKey="step-increment" title="Step Increment Salary">
+        <PersonalSummary />
+      </Tab>
+      <Tab eventKey="loyalty" title="Eligible for Loyalty Summary">
+        <PersonalSummary />
+      </Tab>
+      
+    </Tabs> */}
+      <PersonalSummary />
+      <hr className="line-division"></hr>
+      <DesignationSummary />
     </main>
   );
 }
--- src/components/dashboard/DesignationSummary.jsx
+++ src/components/dashboard/DesignationSummary.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Dropdown from 'react-bootstrap/Dropdown';
 
 const DesignationSummary = () => {
   return (
@@ -17,56 +18,80 @@ const DesignationSummary = () => {
             <input type="text" className="form-control" value="Lumbia National High School" />
           </div>
 
-          
+          <Dropdown>
+            <Dropdown.Toggle variant="success" id="dropdown-basic" className="district-dropdown">
+              Level
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              <Dropdown.Item href="#/action-1">Kinder</Dropdown.Item>
+              <Dropdown.Item href="#/action-2">Elementary</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Junior High</Dropdown.Item>
+              <Dropdown.Item href="#/action-1">Senior High</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+
         <table className="table table-bordered">
           <thead>
             <tr>
+              <th scope="col">Level</th>
               <th scope="col">Designation</th>
               <th scope="col">No. of Employees</th>
             </tr>
           </thead>
           <tbody>
             <tr>
+              <td scope="col">Junior High</td>
               <td scope="col">Principal</td>
               <td scope="col">1</td>
             </tr>
             <tr>
+              <td scope="col">Junior High</td>
               <td scope="col">Master Teacher III</td>
               <td scope="col">0</td>
             </tr>
             <tr>
+              <td scope="col">Junior High</td>
               <td scope="col">Master Teacher II</td>
               <td scope="col">1</td>
             </tr>
             <tr>
+              <td scope="col">Junior High</td>
               <td scope="col">Master Teacher I</td>
               <td scope="col">1</td>
             </tr>
             <tr>
+              <td scope="col">Junior High</td>
               <td scope="col">Special Education Teacher III</td>
               <td scope="col">0</td>
             </tr>
             <tr>
+              <td scope="col">Junior High</td>
               <td scope="col">Special Education Teacher II</td>
               <td scope="col">0</td>
             </tr>
             <tr>
+              <td scope="col">Junior High</td>
               <td scope="col">Special Education Teacher I</td>
               <td scope="col">1</td>
             </tr>
             <tr>
+              <td scope="col">Junior High</td>
               <td scope="col">Special Science Teacher I</td>
               <td scope="col">4</td>
             </tr>
             <tr>
+              <td scope="col">Junior High</td>
               <td scope="col">Teacher III</td>
               <td scope="col">30</td>
             </tr>
             <tr>
+              <td scope="col">Junior High</td>
               <td scope="col">Teacher II</td>
               <td scope="col">20</td>
             </tr>
             <tr>
+              <td scope="col">Junior High</td>
               <td scope="col">Teacher I</td>
               <td scope="col">40</td>
             </tr>
